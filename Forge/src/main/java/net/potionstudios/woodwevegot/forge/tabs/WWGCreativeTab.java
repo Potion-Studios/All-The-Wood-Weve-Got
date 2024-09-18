9c4c991fed82cd4323f7caa0e33955c07ec4bb47
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-09-17 20:15:31 -0400

    Add All items to bottom of Creative tab in Forge

--- Forge/src/main/java/net/potionstudios/woodwevegot/forge/tabs/WWGCreativeTab.java
+++ Forge/src/main/java/net/potionstudios/woodwevegot/forge/tabs/WWGCreativeTab.java
@@ -2,14 +2,14 @@ package net.potionstudios.woodwevegot.forge.tabs;
 
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.potionstudios.biomeswevegone.world.item.BWGCreativeTabs;
-import net.potionstudios.woodwevegot.world.level.block.WWGWoodSet;
+import net.potionstudios.woodwevegot.world.level.block.WWGBlocks;
 
-public class WWGCreativeTabs {
+public class WWGCreativeTab {
 
     public static void addToCreativeTab(final BuildCreativeModeTabContentsEvent event) {
         // Add items to the creative tab
         if (event.getTab() == BWGCreativeTabs.WOOD_TAB.get()) {
-            event.accept(WWGWoodSet.getWoodSets().get(0).barrel());
+            WWGBlocks.BLOCK_ITEMS.forEach(event::accept);
         }
     }
 
