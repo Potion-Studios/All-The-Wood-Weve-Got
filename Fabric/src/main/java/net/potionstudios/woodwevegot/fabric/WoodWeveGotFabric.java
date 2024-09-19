e23fb017daf46b77a124ba59647dce6b93ec494e
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-09-19 16:14:21 -0400

    Move Creative tab stuff to Platform Main classes

--- Fabric/src/main/java/net/potionstudios/woodwevegot/fabric/WoodWeveGotFabric.java
+++ Fabric/src/main/java/net/potionstudios/woodwevegot/fabric/WoodWeveGotFabric.java
@@ -1,10 +1,13 @@
 package net.potionstudios.woodwevegot.fabric;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.potionstudios.biomeswevegone.fabric.BiomesWeveGoneFabric;
+import net.potionstudios.biomeswevegone.world.item.BWGCreativeTabs;
 import net.potionstudios.woodwevegot.WoodWeveGot;
 import net.fabricmc.api.ModInitializer;
-import net.potionstudios.woodwevegot.fabric.integration.BlockIntegration;
-import net.potionstudios.woodwevegot.fabric.tabs.WWGCreativeTab;
+import net.potionstudios.woodwevegot.tags.WWGItemTags;
+import net.potionstudios.woodwevegot.world.level.block.WWGWoodSet;
 
 /**
  * This class is the entrypoint for the mod on the Fabric platform.
@@ -15,7 +18,16 @@ public class WoodWeveGotFabric implements ModInitializer {
     public void onInitialize() {
         BiomesWeveGoneFabric.initializeBiomesWeveGone(WoodWeveGot.MOD_ID);
         WoodWeveGot.init();
-        BlockIntegration.init();
-        WWGCreativeTab.addToCreativeTab();
+        registerFuels();
+        ItemGroupEvents.modifyEntriesEvent(BWGCreativeTabs.WOOD_TAB).register((group) -> WWGWoodSet.getWoodSets()
+                .forEach((wwgWoodSet -> group.addAfter(wwgWoodSet.getWoodSet().chestBoatItem().get(), wwgWoodSet.ladder(), wwgWoodSet.barrel(), wwgWoodSet.chest(), wwgWoodSet.trappedChest()))));
     }
+
+    private static void registerFuels() {
+        FuelRegistry.INSTANCE.add(WWGItemTags.BARRELS, 300);
+        FuelRegistry.INSTANCE.add(WWGItemTags.LADDERS, 300);
+        FuelRegistry.INSTANCE.add(WWGItemTags.CHESTS, 300);
+        FuelRegistry.INSTANCE.add(WWGItemTags.TRAPPED_CHESTS, 300);
+    }
+
 }
--- Forge/src/main/java/net/potionstudios/woodwevegot/forge/WoodWeveGotForge.java
+++ Forge/src/main/java/net/potionstudios/woodwevegot/forge/WoodWeveGotForge.java
@@ -1,13 +1,16 @@
 package net.potionstudios.woodwevegot.forge;
 
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.potionstudios.biomeswevegone.world.item.BWGCreativeTabs;
 import net.potionstudios.woodwevegot.WoodWeveGot;
 import net.minecraftforge.fml.common.Mod;
 import net.potionstudios.woodwevegot.forge.client.WoodWeveGotClientForge;
-import net.potionstudios.woodwevegot.forge.tabs.WWGCreativeTab;
+import net.potionstudios.woodwevegot.world.level.block.WWGWoodSet;
 
 /**
  * Main class for the mod on the Forge platform.
@@ -19,6 +22,16 @@ public class WoodWeveGotForge {
         WoodWeveGot.init();
         ForgePlatformHandler.register(MOD_BUS);
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> WoodWeveGotClientForge.init(MOD_BUS));
-        MOD_BUS.addListener(WWGCreativeTab::addToCreativeTab);
+        MOD_BUS.addListener(WoodWeveGotForge::addToCreativeTab);
+    }
+
+    private static void addToCreativeTab(final BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == BWGCreativeTabs.WOOD_TAB)
+            WWGWoodSet.getWoodSets().forEach((wwgWoodSet -> {
+                event.getEntries().putAfter(wwgWoodSet.getWoodSet().chestBoatItem().get().getDefaultInstance(), wwgWoodSet.ladder().asItem().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                event.getEntries().putAfter(wwgWoodSet.ladder().asItem().getDefaultInstance(), wwgWoodSet.barrel().asItem().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                event.getEntries().putAfter(wwgWoodSet.barrel().asItem().getDefaultInstance(), wwgWoodSet.chest().asItem().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+                event.getEntries().putAfter(wwgWoodSet.chest().asItem().getDefaultInstance(), wwgWoodSet.trappedChest().asItem().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+            }));
     }
 }
