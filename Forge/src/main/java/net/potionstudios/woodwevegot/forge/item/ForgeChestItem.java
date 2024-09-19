29f447e1eeebc135731f0ca903547999a2a86362
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-09-19 15:50:20 -0400

    Move Item Renderer inside of Call on ForgeChestItem

--- Forge/src/main/java/net/potionstudios/woodwevegot/forge/item/ForgeChestItem.java
+++ Forge/src/main/java/net/potionstudios/woodwevegot/forge/item/ForgeChestItem.java
@@ -1,12 +1,17 @@
 package net.potionstudios.woodwevegot.forge.item;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
-import net.potionstudios.woodwevegot.client.WWGChestItemRenderer;
+import net.potionstudios.woodwevegot.world.level.block.WWGTrappedChestBlock;
 import net.potionstudios.woodwevegot.world.level.block.entities.WWGChestBlockEntity;
+import net.potionstudios.woodwevegot.world.level.block.entities.WWGTrappedChestBlockEntity;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
@@ -27,7 +32,14 @@ public class ForgeChestItem extends ForgeBlockItem {
 		consumer.accept(new IClientItemExtensions() {
 			@Override
 			public BlockEntityWithoutLevelRenderer getCustomRenderer() {
-				return new WWGChestItemRenderer<>(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels(), () -> new WWGChestBlockEntity(BlockPos.ZERO, block.get().defaultBlockState()));
+				return new BlockEntityWithoutLevelRenderer(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels()) {
+					@Override
+					public void renderByItem(@NotNull ItemStack stack, @NotNull ItemDisplayContext displayContext, @NotNull PoseStack poseStack, @NotNull MultiBufferSource buffer, int packedLight, int packedOverlay) {
+						if (block.get() instanceof WWGTrappedChestBlock)
+							Minecraft.getInstance().getBlockEntityRenderDispatcher().renderItem(new WWGTrappedChestBlockEntity(BlockPos.ZERO, block.get().defaultBlockState()), poseStack, buffer, packedLight, packedOverlay);
+						else Minecraft.getInstance().getBlockEntityRenderDispatcher().renderItem(new WWGChestBlockEntity(BlockPos.ZERO, block.get().defaultBlockState()), poseStack, buffer, packedLight, packedOverlay);
+					}
+				};
 			}
 		});
 	}
