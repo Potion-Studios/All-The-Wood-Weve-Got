8131f993fea5f4697d72fdf1073b5320c911fff8
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2024-12-10 18:30:00 -0500

    Update to Loom 1.9, Gradle 8.11.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.gradle.kotlin.dsl.get
 
 plugins {
     id("architectury-plugin") version "3.4-SNAPSHOT"
-    id("dev.architectury.loom") version "1.7-SNAPSHOT" apply false
+    id("dev.architectury.loom") version "1.9-SNAPSHOT" apply false
     id("com.gradleup.shadow") version "8.3.5" apply false
     id("com.hypherionmc.modutils.modpublisher") version "2.+"
     java
