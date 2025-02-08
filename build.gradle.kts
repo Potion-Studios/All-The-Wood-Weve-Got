b95069427a652eba7702c788fe6589f96380071c
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-02-07 20:57:12 -0500

    Gradle 8.12.1, Shadow 8.3.6, Annotations 26.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.gradle.kotlin.dsl.get
 plugins {
     id("architectury-plugin") version "3.4-SNAPSHOT"
     id("dev.architectury.loom") version "1.9-SNAPSHOT" apply false
-    id("com.gradleup.shadow") version "8.3.5" apply false
+    id("com.gradleup.shadow") version "8.3.6" apply false
     id("com.hypherionmc.modutils.modpublisher") version "2.+"
     java
     idea
@@ -60,7 +60,7 @@ subprojects {
             parchment("org.parchmentmc.data:parchment-$minecraftVersion:${project.properties["parchment"]}@zip")
         })
 
-        compileOnly("org.jetbrains:annotations:26.0.1")
+        compileOnly("org.jetbrains:annotations:26.0.2")
         compileOnly("com.google.auto.service:auto-service:1.1.1")
         annotationProcessor("com.google.auto.service:auto-service:1.1.1")
     }
