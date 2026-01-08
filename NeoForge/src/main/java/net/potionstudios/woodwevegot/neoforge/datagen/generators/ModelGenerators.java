f75bc6c523bb171ce817abba1917f585db06fdcd
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-01-07 22:25:53 -0500

    4.3.1 Release
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- NeoForge/src/main/java/net/potionstudios/woodwevegot/neoforge/datagen/generators/ModelGenerators.java
+++ NeoForge/src/main/java/net/potionstudios/woodwevegot/neoforge/datagen/generators/ModelGenerators.java
@@ -10,7 +10,6 @@ import net.minecraft.client.renderer.item.ItemModel;
 import net.minecraft.client.renderer.special.ChestSpecialRenderer;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.Identifier;
-import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
